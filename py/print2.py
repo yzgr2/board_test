27981b075263bfc3b1a355b4850b0726a25f0294
yuzhigang@evergrande.cn <yuzhigang@evergrande.cn>
2017-05-17 08:24:47 +0800

    common python

---
+++ py/print2.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+## 在 python 2.x 下用 python 3 的 print
+from __future__ import print_function
+
+print("77777",end='')
+print("8888",end='')
+
---
+++ py/print_no_newline.py
@@ -0,0 +1,8 @@
+print 123,
+print 145,
+print
+
+import sys
+sys.stdout.write("123")
+sys.stdout.write("456")
+
